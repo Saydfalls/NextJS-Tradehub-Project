b0a28aba975055d26902317f4f5b2821a176b338
Adrian Rodriguez-Cruz <airodcruz2001@gmail.com>
2024-12-07 12:28:11 -0500

    feat: add neon for database management

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,18 @@ import Link from 'next/link';
 import style from '@/app/ui/home.module.css';
 import { rubik } from './ui/fonts';
 import Image from 'next/image';
+import { neon } from '@neondatabase/serverless';
 
 export default function Page() {
+  async function create(formData: FormData) {
+    'use server';
+    // Connect to the Neon database
+    const sql = neon(`${process.env.DATABASE_URL}`);
+    const comment = formData.get('comment');
+    // Insert the comment from the form into the Postgres database
+    await sql('INSERT INTO comments (comment) VALUES ($1)', [comment]);
+  }
+
   return (
     <main className="flex min-h-screen flex-col bg-black p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-sky-700 p-4 md:h-52">
