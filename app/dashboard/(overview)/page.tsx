d2bc75f7e40e2bb9606e6aadf7f6b3e1dbc9431c
Adrian Rodriguez-Cruz <airodcruz2001@gmail.com>
2024-12-09 17:02:06 -0500

    fix: grouped dashboard card components to avoid 'popping' effect

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,20 +1,15 @@
-import { Card } from '@/app/ui/dashboard/cards';
 import IncomeChart from '@/app/ui/dashboard/income-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import CardWrapper from '@/app/ui/dashboard/cards';
 import { poppins } from '@/app/ui/fonts';
-import { fetchLatestInvoices, fetchCardData } from '../../lib/data';
 import { Suspense } from 'react';
-import { IncomeChartSkeleton } from '@/app/ui/skeletons';
+import { 
+  IncomeChartSkeleton,
+  LatestInvoicesSkeleton,
+  CardsSkeleton
+ } from '@/app/ui/skeletons';
 
 export default async function Page() {
-  const latestInvoices = await fetchLatestInvoices();
-  const {
-    numberOfSellers,
-    numberOfInvoices,
-    totalFulfilledInvoices,
-    totalAwaitingInvoices
-  } = await fetchCardData();
-
   return (
     <main className="rounded-xl bg-neutral-900 p-6">
       <h1
@@ -23,20 +18,17 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Card title="Earned" value={totalFulfilledInvoices} type="earned" />
-        <Card
-          title="In Progress"
-          value={totalAwaitingInvoices}
-          type="awaiting"
-        />
-        <Card title="All Invoices" value={numberOfInvoices} type="invoices" />
-        <Card title="Total Sellers" value={numberOfSellers} type="sellers" />
+        <Suspense fallback={ <CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<IncomeChartSkeleton />}>
           <IncomeChart />
         </Suspense>
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
